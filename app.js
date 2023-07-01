bfbf335de64e47f1b0bbc2821ef36219d8577073
LucasFromDK <lucashjerbechlarsen@gmail.com>
2023-07-01 18:41:36 +0200

    Added ability to update Text and Images while app is running

--- app.js
+++ app.js
@@ -1,34 +1,56 @@
-const { clientId, refreshTime } = require('./config.json')
-const DiscordRPC = require("discord-rpc")
-const RPC = new DiscordRPC.Client({ transport:"ipc" })
-const startTime = Date.now()
-var hour;
-var minute;
+const fs = require("fs");
+const { clientId, refreshTime } = require("./config.json");
+const DiscordRPC = require("discord-rpc");
+const RPC = new DiscordRPC.Client({ transport:"ipc" });
+const startTime = Date.now();
+var stateText, detailText, largeImageText, smallImageText;
+var largeImageKey, smallImageKey;
+var hour, minute;
+
+function updateText() {
+    var jsonData = fs.readFileSync("./dynamicText.json", "utf8"); //To update the text, change value of the field in dynamicText.json
+    var parsedData = JSON.parse(jsonData);
+    stateText = parsedData.stateText;
+    detailText = parsedData.detailText;
+    largeImageText = parsedData.largeImageText;
+    smallImageText = parsedData.smallImageText;
+    return stateText, detailText, largeImageText, smallImageText;
+  }
+
+function updateImage() {
+    var jsonData = fs.readFileSync("./dynamicImages.json", "utf8"); //To update the Image, change value of the field in dynamicImages.json
+    var parsedData = JSON.parse(jsonData);
+    largeImageKey = parsedData.largeImageKey
+    smallImageKey = parsedData.smallImageKey
+    return largeImageKey, smallImageKey;
+}
 
 async function updateTimestamp() {
-    hour = new Date().getHours()
-    minute = new Date().getMinutes()
+    hour = new Date().getHours();
+    minute = new Date().getMinutes();
     //Makes sure time format is hh:mm and not hh:m
     if (minute >= 0 && minute < 10) {
-        minute = `0${minute}`
+        minute = `0${minute}`;
     }
     //Makes sure time format is hh:mm and not h:mm
     if (hour >= 0 && hour < 10) {
-        hour = `0${hour}`
+        hour = `0${hour}`;
     }
-    return hour, minute
+    return hour, minute;
 }
 
 async function setActivity() {
-    if (!RPC) return
+    if (!RPC) return;
+    updateText();
+    updateImage();
     RPC.setActivity({
-        details: "Welcome to my profile!",
-        state: "Playing with RPC",
+        details: `${detailText}`,
+        state: `${stateText}`,
         startTimestamp: startTime,
-        largeImageKey: "largeimage", //If you want a gif here, replace largeimage with the gif link.
-        largeImageText: "Lorem Ipsum",
-        smallImageKey: "smallimage", //If you want a gif here, replace smallimage with the gif link.
-        smallImageText: "Lorem Ipsum",
+        largeImageKey: `${largeImageKey}`, //If you want a gif here, replace largeimage with the gif link.
+        largeImageText: `${largeImageText}`,
+        smallImageKey: `${smallImageKey}`, //If you want a gif here, replace smallimage with the gif link.
+        smallImageText: `${smallImageText}`,
         instance: false,
         buttons: [
             {
@@ -40,18 +62,18 @@ async function setActivity() {
                 url: "https://lucasfromdk.github.io/"
             }
         ]
-    })
+    });
 }
 
 RPC.on("ready", async() => {
-    setActivity()
+    setActivity();
     setInterval(() => {
-        setActivity()
-        updateTimestamp()
-        console.log(` ${hour}:${minute} | RPC Updated Successfully! Next Update in ${refreshTime} seconds.`)
+        setActivity();
+        updateTimestamp();
+        console.log(` ${hour}:${minute} | RPC Updated Successfully! Next Update in ${refreshTime} seconds.`);
     }, refreshTime*1000) //Refresh Delay, refreshTime being in seconds.
-    return hour, minute
+    return hour, minute;
 })
 
-RPC.login({clientId}).catch(console.error(), updateTimestamp())
-console.log(` ${hour}:${minute} | RPC Started Successfully! RPC Status updates every ${refreshTime} seconds, which is equal to ${refreshTime/60} minutes.`)
+RPC.login({clientId}).catch(console.error(), updateTimestamp());
+console.log(` ${hour}:${minute} | RPC Started Successfully! RPC Status updates every ${refreshTime} seconds, which is equal to ${refreshTime/60} minutes.`);
